d1353e92d431a32933574f5cad193b2404f71ddd
YASH TEKRIWAL <yt2c@virginia.edu>
2016-06-12 23:00:52 +0200

    scraping attempts

---
+++ old script.R
@@ -0,0 +1,12 @@
+old script
+
+#grabbing just the headlines and making one character string 
+
+#text <- ""
+# for (i in 1:length(out$data))
+# { 
+#   change_to_text <- as.character(all_articles[[i]])
+#   result <- change_to_text[2]
+#   text <- paste(text, result)
+# }
+# text <- gsub("(^\\s)", "", text)} 
