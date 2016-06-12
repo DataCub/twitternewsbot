508e6f00601ff04fd928ae9df2cc29020c0009bd
YASH TEKRIWAL <yt2c@virginia.edu>
2016-06-12 17:21:06 +0200

    editing tweeting function

--- scripting.R
+++ scripting.R
@@ -10,6 +10,16 @@ text <- get_text()
 poem <- generate_poem(text)
 
 # send it to twitter 
+library(twitteR)
+library(base64enc)
+library(httr)
+
+consumer_key <- "q2BZQghCoHtBIHus9DCUzIlXS"
+consumer_secret <- "IS5uHBUJZRQfRZFrpsHWT67I0yerAJWOjc6zhe8qG8Q1CNqaxa"
+access_token <- "741928314323996672-xvqmP4z6rGZLmBNdbrfi1zMc86SAwj5"
+access_secret <- "z6WOaRa4YeTODwaFvb8E9x5ap8iI4FDVS7iIk97QU28Q8"
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+
 tweet(poem)
 
 quit(save = "no")
