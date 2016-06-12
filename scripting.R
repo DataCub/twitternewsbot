4109dc6c8d1069859375fcbc0a3576389968b3a3
YASH TEKRIWAL <yt2c@virginia.edu>
2016-06-12 15:51:57 +0200

    Final changes before automation

--- scripting.R
+++ scripting.R
@@ -1,5 +1,15 @@
+# read in functions 
+source(file = "yash_script.R")
+source(file = "TribbyTextGenerate.R")
+source(file = "tweeting.R")
+
 # grab the text 
 text <- get_text()
 
 # generate the poem 
-poem <- generate_poem()
+poem <- generate_poem(text)
+
+# send it to twitter 
+tweet(poem)
+
+quit(save = "no")
--- tweeting.R
+++ tweeting.R
@@ -1,11 +1,11 @@
-library(lubridate)
-
-forever = TRUE
-while(forever){ 
-#Yash's function, got to make this a function
-text <- get_text()
-#Matt's function
-generate_poem(text)
-#Delay
-Sys.sleep(60*60*2)
-}
+tweet <- function(poem) { 
+  library(twitteR)
+  
+  consumer_key = "caY0hvkvH8sb7HzPojUkTrjw1"
+  consumer_secret = "HqXoDX9qqgsxgDD0mjA2V3lO2ao7gp0m5vkINaQTBOfHmaMkmW"
+  access_token = "741928314323996672-IPmVPxhj1CGFHUNXCCWKmPtJuJ4RPBX"
+  access_secret = "QC8fHWCfPvQHtNvS1ub0homvtiMJiR0lQbwp2ezC6YD82"
+  setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+  
+  msg = tweet(poem)
+  }
